17f558b10037085e9797da1dc25e330a47ec8cb6
orlivne <livneor207@gmail.com>
2023-04-19 09:48:56 +0300

    ...

--- train_hellper.py
+++ train_hellper.py
@@ -217,7 +217,7 @@ def sellect_scheduler(optimizer, training_configuration, data_loader, scheduler_
         steps_per_epoch = len(data_loader)
         scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=max_lr, steps_per_epoch=steps_per_epoch, epochs=epochs_count)
     elif scheduler_name == 'ReduceLROnPlateau':
-        factor = 0.5  # reduce by factor 0.5
+        factor = 0.3  # reduce by factor 0.5
         patience = 2  # epochs
         threshold = 1e-2
         verbose = True
